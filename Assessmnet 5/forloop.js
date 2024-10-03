280b65ea8a48dccb2e347bd183947ebdd70a6c3b
Md.Abdullah Al Rakib <122741559+iamrakib087@users.noreply.github.com>
2024-10-04 00:34:32 +0600

    Add files via upload
    .

---
+++ Assessmnet 5/forloop.js
@@ -0,0 +1,10 @@
+var nums = ['100','300','400','60','40'];
+var sum = 0;
+
+for(var i=0; i < nums.length; i++){
+
+    sum += parseInt(nums[i]);
+
+}
+
+console.log(sum);
---
+++ Assessmnet 5/function.js
@@ -0,0 +1,7 @@
+function greet() {
+    console.log("Hello World!");
+}
+
+greet();
+
+console.log("Outside function");
