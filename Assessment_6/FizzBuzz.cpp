735aab00c3d3f12d146f24e1d4afcc8459063a49
Md.Abdullah Al Rakib <iamrakib087@gmail.com>
2024-10-07 16:06:44 +0600

    .

---
+++ Assessment_6/FizzBuzz.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+int main(){
+    int i;
+    // Looping 1 to 50
+    for(i=1; i<=50; i++) {
+        // Check multiple of 3 and 5
+        if(i%3==0 && i%5==0){
+            std::cout << "FizzBuzz";
+        }
+    // Check multiple of 3
+        else if(i%3==0){
+            std::cout << "Fizz";
+        }
+    // Check multiple of 5
+        else if(i%5==0){
+            std::cout << "Buzz";
+        }
+    // if no multiple numbers
+    else{
+            std::cout << i;
+        }
+    if(i<50){
+        std::cout << ", ";
+    }
+    }
+    return 0;
+}
