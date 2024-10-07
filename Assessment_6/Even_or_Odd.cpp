57db44738d40ab79e0509718bf71e32900755763
Md.Abdullah Al Rakib <iamrakib087@gmail.com>
2024-10-07 14:54:04 +0600

    .

---
+++ Assessment_6/Even_or_Odd.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int main(){
+    int number;
+    // user input
+    std::cout << "Enter a number: ";
+    std::cin >> number;
+
+    // check if the number is even or odd
+    if(number%2 == 0) {
+        std::cout << number << " is an even number.";}
+    else{
+        std::cout << number << " is an odd number.";
+    }
+}
